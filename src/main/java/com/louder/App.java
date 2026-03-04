13983c07f16e15d1d819743d1b0cc126a78256b1
Mohammed Taiyub V <144512125+MohammedTaiyubV@users.noreply.github.com>
2026-03-04 10:10:13 +0530

    Update App.java

--- src/main/java/com/louder/App.java
+++ src/main/java/com/louder/App.java
@@ -1,14 +1,14 @@
 package com.louder;
-class Fact
-{
-	public static void main(String[]args)
-	{
-	
-		int n = 6 , Fact=1,temp=n;
-		while(n>1)
-		{
-			Fact*=n--;
-		}
-		System.out.println(temp+" Factorial is :"+Fact);
-	}
+
+class Fact {
+    public static void main(String[] args) {
+
+        int n = 6, fact = 1, temp = n;
+
+        while (n > 1) {
+            fact *= n--;
+        }
+
+        System.out.println(temp + " Factorial is : " + fact);
+    }
 }
